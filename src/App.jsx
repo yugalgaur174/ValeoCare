364261f40ae3770ed5878c4152c9d96131c0322f
kanaame <aruniyaasokan@gmail.com>
2024-04-16 22:51:18 +0530

    final home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Button, Container, Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JavaScript
 
 function App() {
   return (
