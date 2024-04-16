eedfa07914e6819e36b72fda71b486a7d92d3748
YUGAL GAUR <yugalgaur174@gmail.com>
2024-04-17 03:05:36 +0530

    linking- successful

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import Home from './pages/Home'
+import React from "react";
+import Home from "./pages/Home.jsx";
+import Forum from "./pages/Forum.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home/>
-    </>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/forum" element={<Forum />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
